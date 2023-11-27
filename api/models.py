d2069484cd0f87d97c4effb7e99b41e291573cd2
Athish Sutharsan <athish@Athishs-MBP.cust.communityfibre.co.uk>
2023-11-27 18:18:24 +0000

    Updated custom user model and made other changes

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     profile_image = models.ImageField(upload_to='profile_images/')
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)   
